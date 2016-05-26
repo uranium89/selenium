365ac00322e8d7977403f2dab8355b90b4716d41
= <=>
2016-05-26 09:26:46 +0700

    update

--- app.js
+++ app.js
@@ -1,8 +1,9 @@
 var snow = require('./servicenow-deployment/auto_deployment');
 var server = process.argv[2];
-var username = process.argv[3];
-var password = process.argv[4];
-var path = process.argv[5];
-var log = process.argv[6];
+var srcServer = process.argv[3];
+var username = process.argv[4];
+var password = process.argv[5];
+var path = process.argv[6];
+var log = process.argv[7];
 
-snow.deploy(server, username, password, path, log);
+snow.deploy(server,srcServer, username, password, path, log);
